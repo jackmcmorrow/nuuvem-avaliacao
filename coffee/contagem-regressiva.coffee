0416a2079d692ecc567cac58e0cd9b9f0bab4871
cav_dan <dcc69eyes@gmail.com>
2013-06-17 09:59:49 -0300

    Agora está contando direito

--- coffee/contagem-regressiva.coffee
+++ coffee/contagem-regressiva.coffee
@@ -2,23 +2,27 @@ hoje = new Date()
 eh_par = (x) -> (x % 2) == 0
 eh_impar = (x) -> not eh_par x
 
-contarDias = (meses, dias) -> #tá ok
+contarMeses = (meses, dias) -> #tá ok
+	@dias = dias
 	if eh_par meses
 		if meses is 2 
-			dias += 28
+			dias = dias + 28
+			#console.log 'fevereiro'
 		else 
-			dias += 31
-	if eh_impar meses 
-		dias += 30
+			dias = dias + 30
+			#console.log 'mes com trinta'
+	else
+		dias = dias + 31
+		#console.log 'mes com trinta e um'
 	dias
 
 atualizarNum = ($obj, val) -> #tá ok
-
-	if val >= 10
+	if val >= 10 
 		alg = val.toString().split("");
 		$($obj + ' .dec').text(alg[0])
 		$($obj + ' .uni').text(alg[1])
 	else
+		$($obj + ' .uni').text('0')	
 		$($obj + ' .uni').text(val)	
 	
 
@@ -32,54 +36,84 @@ class Contador
 		@hMin = hoje.getMinutes() 
 		@hSec = hoje.getSeconds() 
 		@hDia = hoje.getDate()
-		
-		@dias = ->
-			a = 0
+
+		@meses = () ->
+			anos = @hAno
+			fimAnos = @fAno
+			deltaAnos = fimAnos - anos
+			
 			meses = @hMes
-			while @fMes > @hMes
-				contarDias meses, a
-				meses -= 1
-				#console.log a
+			fimMeses = @fMes
+			deltaMeses = @fMes - @hMes
+
+			if fimAnos > anos 
+				deltaMeses += Math.abs deltaAnos * 12
+
+			return deltaMeses
+		
+		@dias = () ->
+			a = 0 #variação de dias em cada mês
+			meses = @hMes
+			fimMeses = @fMes - 1
+			fimDia = @fDia
+
+			while fimMeses > meses
+				a = contarMeses fimMeses, a
+				fimMeses -= 1
+				
+			x = 0 #dias até esse mês acabar
+			x += contarMeses @hMes, x
+			return Math.abs (x - @hDia) + a + fimDia
+
+
+
 
 		#Delta, a variação
-		@dAno 	= @fAno - @hAno
-		@dMes 	= @fMes - @hMes
-		@dDia 	= @dias;
-		@dHoras = @fHoras - @hHoras
-		@dMin 	= @fMin - @hMin
-		@dSec 	= @fSec - @hSec
+		@dAno 	= Math.abs @fAno - @hAno
+		@dMes 	= Math.abs @fMes - @hMes
+		@dDia 	= @dias()
+		@dHoras = Math.abs @fHoras - @hHoras
+		@dMin 	= Math.abs @fMin - @hMin
+		@dSec 	= Math.abs @fSec - @hSec
 
 		
 
 		console.log @hAno + ' ' + @hMes + ' ' + @hDia + ' ' + @hHoras + ' ' + @hMin + ' ' + @hSec
+		console.log @fAno + ' ' + @fMes + ' ' + @fDia + ' ' + @fHoras + ' ' + @fMin + ' ' + @fSec
+		console.log @dAno + ' ' + @dMes + ' ' + @dDia + ' ' + @dHoras + ' ' + @dMin + ' ' + @dSec
 		
 		atualizarNum '#segundos', @dSec
 		atualizarNum '#minutos', @dMin
-		atualizarNum '#horas', @dSec
-		atualizarNum '#dias', @dias
+		atualizarNum '#horas', @dHoras
+		atualizarNum '#dias', @dDia
 
 		@contar = () ->
-			diasFaltando = @dias 0
-			console.log diasFaltando
+			diasFaltando = @dDia
+			horasFaltando = @dHoras
+			minutosFaltando = @dMin
+			segundosFaltando = @dSec
+			
 
 			atualizarNum '#dias', diasFaltando
 
-			if @hHoras < 0
-				@dias -= 1
-				@hHoras = 23
-				atualizarNum '#horas', @hHoras
+			if horasFaltando < 0
+				diasFaltando = diasFaltando - 1
+				horasFaltando = 23
+				atualizarNum '#horas', horasFaltando
 
-			if @hMin <= 0
-				@hHoras -= 1
-				@hMin = 60
-				atualizarNum '#minutos', @hMin
+			if minutosFaltando <= 0
+				horasFaltando = horasFaltando - 1
+				minutosFaltando = 60
+				atualizarNum '#minutos', minutosFaltando
 
-			if @hSec <= 0
-				@hMin -= 1
-				@hSec = 60
-			atualizarNum '#segundos', @hSec
+			if segundosFaltando <= 0
+				minutosFaltando = minutosFaltando - 1
+				segundosFaltando = 60
 
-			@hSec -= 1
+			atualizarNum '#segundos', segundosFaltando
+			@segundosFaltando -= 1
+			console.log segundosFaltando
 
 contador = new Contador 12, 8, 2013, 19, 0, 0
-#setTimeout contador.contar(), 100000000
+#contador = new Contador 14, 10, 2014, 19, 0, 0
+#setInterval contador.contar(), 1000
--- js/contagem-regressiva.js
+++ js/contagem-regressiva.js
@@ -1,111 +1,131 @@
-(function() {
-  var Contador, atualizarNum, contador, contarDias, eh_impar, eh_par, hoje;
+var Contador, atualizarNum, contador, contarMeses, eh_impar, eh_par, hoje;
 
-  hoje = new Date();
+hoje = new Date();
 
-  eh_par = function(x) {
-    return (x % 2) === 0;
-  };
+eh_par = function(x) {
+  return (x % 2) === 0;
+};
 
-  eh_impar = function(x) {
-    return !eh_par(x);
-  };
+eh_impar = function(x) {
+  return !eh_par(x);
+};
 
-  contarDias = function(meses, dias) {
-    if (eh_par(meses)) {
-      if (meses === 2) {
-        dias += 28;
-      } else {
-        dias += 31;
-      }
-    }
-    if (eh_impar(meses)) {
-      dias += 30;
-    }
-    return dias;
-  };
-
-  atualizarNum = function($obj, val) {
-    var alg;
-
-    if (val >= 10) {
-      alg = val.toString().split("");
-      $($obj + ' .dec').text(alg[0]);
-      return $($obj + ' .uni').text(alg[1]);
+contarMeses = function(meses, dias) {
+  this.dias = dias;
+  if (eh_par(meses)) {
+    if (meses === 2) {
+      dias = dias + 28;
     } else {
-      return $($obj + ' .uni').text(val);
+      dias = dias + 30;
     }
-  };
+  } else {
+    dias = dias + 31;
+  }
+  return dias;
+};
 
-  Contador = (function() {
-    function Contador(fDia, fMes, fAno, fHoras, fMin, fSec) {
-      var final;
+atualizarNum = function($obj, val) {
+  var alg;
 
-      this.fDia = fDia;
-      this.fMes = fMes;
-      this.fAno = fAno;
-      this.fHoras = fHoras;
-      this.fMin = fMin;
-      this.fSec = fSec;
-      final = new Date(this.fAno, this.fMes, this.fDia, this.fHoras, this.fMin, this.fSec);
-      this.hAno = hoje.getFullYear();
-      this.hMes = hoje.getMonth() + 1;
-      this.hHoras = hoje.getHours();
-      this.hMin = hoje.getMinutes();
-      this.hSec = hoje.getSeconds();
-      this.hDia = hoje.getDate();
-      this.dias = function() {
-        var a, meses, _results;
+  if (val >= 10) {
+    alg = val.toString().split("");
+    $($obj + ' .dec').text(alg[0]);
+    return $($obj + ' .uni').text(alg[1]);
+  } else {
+    $($obj + ' .uni').text('0');
+    return $($obj + ' .uni').text(val);
+  }
+};
 
-        a = 0;
-        meses = this.hMes;
-        _results = [];
-        while (this.fMes > this.hMes) {
-          contarDias(meses, a);
-          _results.push(meses -= 1);
-        }
-        return _results;
-      };
-      this.dAno = this.fAno - this.hAno;
-      this.dMes = this.fMes - this.hMes;
-      this.dDia = this.dias;
-      this.dHoras = this.fHoras - this.hHoras;
-      this.dMin = this.fMin - this.hMin;
-      this.dSec = this.fSec - this.hSec;
-      console.log(this.hAno + ' ' + this.hMes + ' ' + this.hDia + ' ' + this.hHoras + ' ' + this.hMin + ' ' + this.hSec);
-      atualizarNum('#segundos', this.dSec);
-      atualizarNum('#minutos', this.dMin);
-      atualizarNum('#horas', this.dSec);
-      atualizarNum('#dias', this.dias);
-      this.contar = function() {
-        var diasFaltando;
+Contador = (function() {
+  function Contador(fDia, fMes, fAno, fHoras, fMin, fSec) {
+    var final;
 
-        diasFaltando = this.dias(0);
-        console.log(diasFaltando);
-        atualizarNum('#dias', diasFaltando);
-        if (this.hHoras < 0) {
-          this.dias -= 1;
-          this.hHoras = 23;
-          atualizarNum('#horas', this.hHoras);
-        }
-        if (this.hMin <= 0) {
-          this.hHoras -= 1;
-          this.hMin = 60;
-          atualizarNum('#minutos', this.hMin);
-        }
-        if (this.hSec <= 0) {
-          this.hMin -= 1;
-          this.hSec = 60;
-        }
-        atualizarNum('#segundos', this.hSec);
-        return this.hSec -= 1;
-      };
-    }
+    this.fDia = fDia;
+    this.fMes = fMes;
+    this.fAno = fAno;
+    this.fHoras = fHoras;
+    this.fMin = fMin;
+    this.fSec = fSec;
+    final = new Date(this.fAno, this.fMes, this.fDia, this.fHoras, this.fMin, this.fSec);
+    this.hAno = hoje.getFullYear();
+    this.hMes = hoje.getMonth() + 1;
+    this.hHoras = hoje.getHours();
+    this.hMin = hoje.getMinutes();
+    this.hSec = hoje.getSeconds();
+    this.hDia = hoje.getDate();
+    this.meses = function() {
+      var anos, deltaAnos, deltaMeses, fimAnos, fimMeses, meses;
 
-    return Contador;
+      anos = this.hAno;
+      fimAnos = this.fAno;
+      deltaAnos = fimAnos - anos;
+      meses = this.hMes;
+      fimMeses = this.fMes;
+      deltaMeses = this.fMes - this.hMes;
+      if (fimAnos > anos) {
+        deltaMeses += Math.abs(deltaAnos * 12);
+      }
+      return deltaMeses;
+    };
+    this.dias = function() {
+      var a, fimDia, fimMeses, meses, x;
 
-  })();
+      a = 0;
+      meses = this.hMes;
+      fimMeses = this.fMes - 1;
+      fimDia = this.fDia;
+      while (fimMeses > meses) {
+        a = contarMeses(fimMeses, a);
+        fimMeses -= 1;
+      }
+      x = 0;
+      x += contarMeses(this.hMes, x);
+      return Math.abs((x - this.hDia) + a + fimDia);
+    };
+    this.dAno = Math.abs(this.fAno - this.hAno);
+    this.dMes = Math.abs(this.fMes - this.hMes);
+    this.dDia = this.dias();
+    this.dHoras = Math.abs(this.fHoras - this.hHoras);
+    this.dMin = Math.abs(this.fMin - this.hMin);
+    this.dSec = Math.abs(this.fSec - this.hSec);
+    console.log(this.hAno + ' ' + this.hMes + ' ' + this.hDia + ' ' + this.hHoras + ' ' + this.hMin + ' ' + this.hSec);
+    console.log(this.fAno + ' ' + this.fMes + ' ' + this.fDia + ' ' + this.fHoras + ' ' + this.fMin + ' ' + this.fSec);
+    console.log(this.dAno + ' ' + this.dMes + ' ' + this.dDia + ' ' + this.dHoras + ' ' + this.dMin + ' ' + this.dSec);
+    atualizarNum('#segundos', this.dSec);
+    atualizarNum('#minutos', this.dMin);
+    atualizarNum('#horas', this.dHoras);
+    atualizarNum('#dias', this.dDia);
+    this.contar = function() {
+      var diasFaltando, horasFaltando, minutosFaltando, segundosFaltando;
 
-  contador = new Contador(12, 8, 2013, 19, 0, 0);
+      diasFaltando = this.dDia;
+      horasFaltando = this.dHoras;
+      minutosFaltando = this.dMin;
+      segundosFaltando = this.dSec;
+      atualizarNum('#dias', diasFaltando);
+      if (horasFaltando < 0) {
+        diasFaltando = diasFaltando - 1;
+        horasFaltando = 23;
+        atualizarNum('#horas', horasFaltando);
+      }
+      if (minutosFaltando <= 0) {
+        horasFaltando = horasFaltando - 1;
+        minutosFaltando = 60;
+        atualizarNum('#minutos', minutosFaltando);
+      }
+      if (segundosFaltando <= 0) {
+        minutosFaltando = minutosFaltando - 1;
+        segundosFaltando = 60;
+      }
+      atualizarNum('#segundos', segundosFaltando);
+      this.segundosFaltando -= 1;
+      return console.log(segundosFaltando);
+    };
+  }
 
-}).call(this);
+  return Contador;
+
+})();
+
+contador = new Contador(12, 8, 2013, 19, 0, 0);
